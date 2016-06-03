d68df350365d76f5a17617893fa59d44cfb9f272
mingtao <madebyrag@qq.com>
2016-06-03 14:13:28 +0800

    cannot wirte all

--- source/os.asm
+++ source/os.asm
@@ -1,178 +1,117 @@
 ; haribote-os
 ; TAB=4
-struc   file
-.name      RESB    1
-.type       RESB    1
-.no       RESB    1
-.next       RESB    1
-.data RESB 60
-.size:
-endstruc
 
 
-		ORG		0x8200			;
-        segment .bss
-iArr: RESB file.size * 8
-iArr_l: EQU ($-iArr)/file.size
+ORG     0x8200
+;section .data
+fatContent:
+    DB      0xeb, 0x4e, 0x90
+    DB      "HUSTSOFT"      ; 启动区的名称可以是任意字符串（8字节）
+    DW      512             ; 每个扇区的大小（必须为512字节）
+    DB      1               ; 簇的大小（必须为1字节）
+    DW      1               ; FAT的起始位置（一般从第一个扇区开始）
+    DB      2               ; FAT的个数（必须为2）
+    DW      224             ; 根目录大小（一般设成224项）
+    DW      2880            ; 该磁盘的大小（必须是2880扇区）
+    DB      0xf0            ; 磁盘的种类（必须是0xf0）
+    DW      9               ; FAT的长度（必须是9扇区）
+    DW      18              ; 1个磁道的扇区数（必须是18）
+    DW      2               ; 磁头数（必须是2）
+    DD      0               ; 不使用分区（必须是0）
+    DD      2880            ; 重写一次磁盘大小
+    DB      0,0,0x29        ; 意义不明，固定
+    DD      0xffffffff      ; 卷标号码
+    DB      "HUSTSOFTOS "   ; 磁盘名称（11字节）
+    DB      "FAT12   "      ; 磁盘格式名称（8字节）
+    RESB    0x0174-($-fatContent)
+    ;RESB    0x1fe-($-$$)
+    DB      0x55, 0xaa
+xcz:
+    RESB    0xa
+    DB  "hahaha"
 
-segment .text
-;MOV AL,0x13;
- ;       MOV AH,0x00
-  ;              INT 0x10
-
-mov cx, iArr_l
-mov si, iArr
-
-L1:
-mov al, 'a'
-mov [si+file.name], al
-mov al, 'b'
-mov [si+file.type], al
-mov al, 'c'
-mov [si+file.no], al
-mov al, 0x02
-mov [si+file.next], al
-mov al, 'c'
-;mov [si+file.data], al
-add  si, file.size 
-loop L1
-    
-
-int 0x11
-;mov al, ah
-;add al ,0x30
-mov ah, 0x0e
-int 0x10
+;section .text
 
 mov si, 0
-
 retry:
-mov al, '7'
-MOV AH,0x0e
-int 0x10
+    mov ax, 0
+    mov es, ax
+    mov ch, 0
+    mov dh, 0
+    mov cl, 1       ;初始扇区是cl=1 而不是0
+
+    mov ah, 0x03
+    mov al, 1
+    mov bx, fatContent
+    mov dl, 0x01
+    int 0x13
+    jnc next
 
 
-         mov ax, 0x0820
-mov es, ax
-mov ch, 0
-mov dh, 0
-mov cl, 2
+    add si, 1
+    cmp si, 5
 
-mov ah, 0x03
-mov al, 1
-mov bx, 0
-mov dl, 0x01
-int 0x13
-jnc next
+    jae error
 
+    MOV AH,0x00
+    MOV DL,0x01; A驱动器
+    INT 0x13; 重置驱动器
 
-add si, 1
-cmp si, 5
-
-
-jae error
-
-MOV AH,0x00
-MOV DL,0x01; A驱动器
-INT 0x13; 重置驱动器
-
-jmp retry
+    jmp retry
 
 error:
-mov al, ah
-add al ,0x30
-MOV AH,0x0e
-int 0x10
+    mov al, ah
+    ;add al ,0x30
+    MOV AH,0x0e
+    int 0x10
+
+    mov ah, 0x0e
+    mov al, ' '
+    int 0x10
+    mov al, 'e'
+    int 0x10
+    mov al, 'r'
+    int 0x10
+    mov al, 'r'
+    int 0x10
+    mov al, ' '
+    int 0x10
 
 next:
 
-;mov ax, es
-;add ax, 0x0020
-;mov es, ax
-;mov ch, 0
-;mov dh, 0
-;mov cl, 5
+    mov bx, es
+    mov al, [0x8200]
+    MOV AH,0x0e
+    int 0x10
+    mov al, [0x8400]
+    MOV AH,0x0e
+    int 0x10
 
-;mov ah, 0x02
-;mov al, 1
-;mov bx, 0
-;mov dl, 0x00
-;int 0x13
+    mov al, [0x8600]
+    MOV AH,0x0e
+    int 0x10
 
+jmp boot
 
-mov bx, es
-mov al, [0x8200]
-MOV AH,0x0e
-int 0x10
-mov al, [0x8400]
-MOV AH,0x0e
-int 0x10
-
-mov al, [0x8600]
-MOV AH,0x0e
-int 0x10
-
-
-
-
-
-jmp short boot
-
-
-
-
-
-
-
-bootmesg db "Our OS boot sector loading ......"
-
-print_mesg : 
-mov ah,0x13 
-mov al,0x00 
-mov bx,0x0007 
-mov cx,0x20 
-mov dx,0x0000 
-int 0x10 
-ret 
-
-get_key :
-mov ah,0x00
-int 0x16 
-MOV AH,0x0e
-int 0x10
-;jmp get_key
-ret
-
-clrscr : 
-mov ax,0x0600 
-mov cx,0x0000 
-mov dx,0x174f 
-mov bh,0
-int 0x10
-ret 
+log:
+    mov ah, 0x0e
+    mov al, ' '
+    int 0x10
+    mov al, 'l'
+    int 0x10
+    mov al, 'o'
+    int 0x10
+    mov al, 'g'
+    int 0x10
+    mov al, ' '
+    int 0x10
+    ret
 
 boot:
-    
-;    mov bp,bootmesg
-;    call print_mesg
-;    call get_key
-MOV AH,0x0e
-    mov al, 'H'
-    INT 0x10
-    mov al, 'U'
-    INT 0x10
-    mov al, 'S'
-    INT 0x10
-    mov al, 'T'
-    INT 0x10
-    call get_key
-
-
-mov ah, 0x24
-mov al, 1
-int 15h
-
+    call log
 
 fin:
 		HLT
 		JMP		fin
+
+
